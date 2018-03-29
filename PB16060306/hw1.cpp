94d6e89c3749b42cb3d2995310d16c3d067d6fb2
zqf0722 <37830130+zqf0722@users.noreply.github.com>
2018-03-29 11:18:06 +0800

    Update hw1.cpp
    完成了除词组外的功能

--- PB16060306/hw1.cpp
+++ PB16060306/hw1.cpp
@@ -1,5 +1,3 @@
-//
-
 #include "stdafx.h"
 #include<stdio.h>
 #include<vector>
@@ -11,29 +9,67 @@
 
 using namespace std;
 
+typedef struct BiTNode {
+	char best[58];
+	int times;
+	int isin = 0;
+	struct BiTNode *lchild, *rchild;
+}BiTNode, *BiTree;
+
+typedef struct most {
+	char name[58];
+	int times;
+}most;
+
+most word[10];
+
 int char_number = 0; int line_number = 0; int word_number = 0;	//字符数 行数 单词数
 
 void listFiles(const char * dir);
 
+
 bool isenglish(char x);
 
-typedef struct node {
-	string key;
-	int num;
-	string best;
-}Node;
+
+bool keycmp(char *s1, char *s2);
+
+void change(char *s);
+
+void addtree(BiTree s, BiTree &T);
 
 
-int main()
+BiTree T=NULL;
+
+int main(int argc, char **argv)
 {
-	Node word[10000000];
+	for (int i = 0; i < 10; i++)
+	{
+		word[i].times = 0;
+	}
 	char dir[200];
-	cout << "Enter a directory: ";
-	cin.getline(dir, 200);
 
+	strcpy(dir, argv[1]);
+	char dirname[200];
+	strcpy(dirname, dir);
+	strcat(dirname, "\\Result.txt");
+	ofstream mycout(dirname);
 	listFiles(dir);
-	printf("%d\n", char_number);
-	printf("%d\n", line_number);
+	mycout << "char_number :" << char_number << endl;
+//	printf("%d\n", char_number);
+	mycout << "line_number :" << line_number << endl;
+//	printf("%d\n", line_number);
+	mycout << "word_number :" << word_number << endl;
+//	cout << word_number << endl;
+	for (int i = 9; i >= 0; i--)
+	{
+		mycout << word[i].name << '\t' << word[i].times << endl;
+		//		cout << word[i].name << '\t' << word[i].times << endl;
+	}
+//	while (L->next != NULL)
+//	{
+//		cout << L->next->key << endl;
+//		L = L->next;
+//	}
 	system("pause");
 	return 0;
 }
@@ -81,9 +117,65 @@ void listFiles(const char * dir)
 								char_number++;
 							if (temp[i] == 13 || temp[i] == 10)
 								line_number++;
-						//	if(isenglish(temp[i]))
 						}
 						line_number++;
+						int flag = 0;
+						for (int j = 0; temp[j] != NULL; j++)
+						{
+							if (isenglish(temp[j]))
+							{
+								char p[58];
+								p[flag] = temp[j];
+								flag++;
+								int t = j+1;
+								for (;; t++)
+								{
+									if (isenglish(temp[t]))
+									{
+										p[flag] = temp[t];
+										flag++;
+									}
+									else
+									{
+										for (; temp[t] >= 48 && temp[t] <= 57; t++);
+										if (isenglish(temp[t]))
+											word_number++;
+										p[flag] = '\0';
+										break;
+									}
+								}
+								if (flag >= 4)	//说明是英语单词
+								{
+									//cout << p << endl;
+									char s1[58];
+									strcpy(s1, p);
+									BiTree n;
+									n = new BiTNode;
+									strcpy(n->best, s1);
+									n->times = 1;
+									n->lchild = NULL;
+									n->rchild = NULL;
+									addtree(n, T);
+									for (int i = 0; p[i] != '\0'; i++)
+									{
+										p[i] = '\0';
+									}
+									j = t-1;
+									flag = 0;
+									continue;
+								}
+								else  //说明不是英语单词
+								{
+									for (int i = 0; p[i] != '\0'; i++)
+									{
+										p[i] = '\0';
+									}
+									j = t-1;
+									flag = 0;
+									continue;
+								}
+							}
+						}
 			}
 			myfile.close();
 		}	
@@ -94,7 +186,106 @@ void listFiles(const char * dir)
 }
 bool isenglish(char x)
 {
-	if (x <= 122 && x >= 41) return 1;
+	if (x <= 90 && x >= 65) return 1;
+	else if (x >= 97 && x <= 122) return 1;
 	else return 0;
+}
+
+void change(char *s)
+{
+	for (int i = 0; s[i] != NULL; i++)
+	{
+		if (s[i] >= 65 && s[i] <= 90)
+			s[i] = s[i] + 32;
+	}
+}
+bool keycmp(char *s1, char *s2)
+{
+	for (int i = 0; s1[i] != NULL; i++)
+	{
+		if (s1[i]>s2[i])
+		{
+			return 1;
+		}
+		if (s1[i] == s2[i])
+		{
+			continue;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	return 1;
+}	//比较两单词的顺序，best是s1则返回0，否则返回1； 
+
+void order(most *word)
+{
+	most n;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = i; j < 9; j++)
+		{
+			if (word[j].times > word[j + 1].times)
+			{
+				n = word[j + 1];
+				word[j + 1] = word[j];
+				word[j] = n;
+			}
+		}
+	}
+}
+
+void addtree(BiTree n, BiTree &T)
+{
+	char key1[58];
+	char key2[58];
+	strcpy(key2, n->best);
+	change(key2);
+	int i;
+	if (T == NULL)
+	{
+		T = n;
+		if (T->times > word[0].times)
+		{
+				word[0].times = T->times;
+				strcpy(word[0].name, T->best);
+				order(word);
+		}
+		word_number++;
+	}
+	else
+	{
+		strcpy(key1, T->best);
+		change(key1);
+		if (strcmp(key1, key2) > 0)
+			addtree(n, T->lchild);
+		else if (strcmp(key1, key2) < 0)
+			addtree(n, T->rchild);
+		else
+		{
+			T->times++;
+			if (T->times > word[0].times)
+			{
+				for (i = 0; i < 10; i++)
+				{
+					if (strcmp(word[i].name, T->best) == 0)
+					{
+						word[i].times++;
+						order(word);
+						break;
+					}
+				}
+				if (i == 10)
+				{
+					word[0].times = T->times;
+					strcpy(word[0].name, T->best);
+					order(word);
+				}
+			}
+			word_number++;
+			return;
+		}
+	}
 }
 
