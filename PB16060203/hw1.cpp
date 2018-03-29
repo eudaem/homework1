f0bc17880605c2fe1572066759c1c7f616ec7d51
KangXinUstc <35297996+KangXinUstc@users.noreply.github.com>
2018-03-29 21:05:47 +0800

    Update hw1.cpp

--- PB16060203/hw1.cpp
+++ PB16060203/hw1.cpp
@@ -29,7 +29,7 @@ struct wordInfo {
 	int	  presentNextWordNum;
 	int   frequency;
 	int   strlength;
-	int   wordLength;//不包含最后的数字部分
+	int   wordLength;//not include the final digits
 };
 struct alphaArray {
 	wordInfo* wordArray;
@@ -131,40 +131,25 @@ void getFilePath(char* filePath)
 
 void traverseFileandCount(char* path, struct alphaArray* dictionary)
 {
-	DIR *pDir; //定义一个DIR类的指针
-	struct dirent *ent=NULL; //定义一个结构体 dirent的指针，dirent结构体见上
+	DIR *pDir; 
+	struct dirent *ent=NULL; 
 	int i = 0;
-	char childpath[512]; //定义一个字符数组，用来存放读取的路径
-	pDir = opendir(path); // opendir方法打开path目录，并将地址付给pDir指针
-	memset(childpath, 0, sizeof(childpath)); //将字符数组childpath的数组元素全部置零
+	char childpath[512]; 
+	pDir = opendir(path);
+	memset(childpath, 0, sizeof(childpath)); 
 	while ((ent = readdir(pDir)) != NULL)
-		//读取pDir打开的目录，并赋值给ent, 同时判断是否目录为空，不为空则执行循环体
 	{
 		if (ent->d_type&DT_DIR)
-			/*读取 打开目录的文件类型 并与 DT_DIR进行位与运算操作，即如果读取的d_type类型为DT_DIR
-			(=4 表示读取的为目录)*/
 		{
 			if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
-				//如果读取的d_name为 . 或者.. 表示读取的是当前目录符和上一目录符,
-				//则用contiue跳过，不进行下面的输出
 				continue;
 			sprintf(childpath, "%s/%s", path, ent->d_name);
-			//如果非. ..则将 路径 和 文件名d_name 付给childpath, 并在下一行prinf输出
-			//printf("path:%s\n",childpath);原文链接这里是要打印出文件夹的地址
 			traverseFileandCount(childpath, dictionary);
-			//递归读取下层的字目录内容， 因为是递归，所以从外往里逐次输出所有目录（路径+目录名），
-			//然后才在else中由内往外逐次输出所有文件名
 		}
 		else
-			//如果读取的d_type类型不是 DT_DIR, 即读取的不是目录，而是文件，
-			//则直接输出 d_name, 即输出文件名
 		{
-			//cout<<ent->d_name<<endl; 输出文件名
-			//cout<<childpath<<"/"<<ent->d_name<<endl; 输出带有目录的文件名
 			sprintf(childpath, "%s/%s", path, ent->d_name);
-			//你可以唯一注意的地方是下一行
-			//目前childpath就是你要读入的文件的path了，可以作为你的读入文件的函数的参数
-			count(childpath, dictionary);//这里就是你的处理文件的接口！，
+			count(childpath, dictionary);
 		}
 	}
 }
