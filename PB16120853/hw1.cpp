5d1aba2a97a33839c20e7889e2ff015b1e540114
coder <36957222+ruizhao13@users.noreply.github.com>
2018-03-29 17:34:24 +0800

    Update hw1.cpp

--- PB16120853/hw1.cpp
+++ PB16120853/hw1.cpp
@@ -1,84 +1,300 @@
-#include <iostream>  
-#include <string>  
-#include <io.h>  
+#include <iostream>
+#include <string>
+#include <fstream>
+//#include <io.h>
+#include<ctype.h>
+#include <algorithm>
+#include <unordered_map>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <iostream>
 using namespace std;
-
-//遍历当前目录下的文件夹和文件,默认是按字母顺序遍历  
-bool TraverseFiles(string path, int &file_num)
+long long TotalNum_chars = 0;
+long long TotalNum_lines = 0;
+long long TotalNum_words = 0;
+struct my_word
 {
-	_finddata_t file_info;
-	string current_path = path + "/*.*"; //可以定义后面的后缀为*.exe，*.txt等来查找特定后缀的文件，*.*是通配符，匹配所有类型,路径连接符最好是左斜杠/，可跨平台  
-										 //打开文件查找句柄  
-	intptr_t handle = _findfirst(current_path.c_str(), &file_info);
-	//返回值为-1则查找失败  
-	if (-1 == handle)
-		return false;
-	do
-	{
-		//判断是否子目录  
-		string attribute;
-		if (file_info.attrib == _A_SUBDIR) //是目录  
-			attribute = "dir";
-		else
-			attribute = "file";
-		//输出文件信息并计数,文件名(带后缀)、文件最后修改时间、文件字节数(文件夹显示0)、文件是否目录  
-		cout << file_info.name << ' ' << file_info.time_write << ' ' << file_info.size << ' ' << attribute << endl; //获得的最后修改时间是time_t格式的长整型，需要用其他方法转成正常时间显示  
-		file_num++;
+    string sort_word = "zzzzzzzzzzzzzzzzzz";
+    size_t appear_count = 0;
+};
 
-	} while (!_findnext(handle, &file_info));  //返回0则遍历完  
-											   //关闭文件句柄  
-	_findclose(handle);
-	return true;
+my_word ten_word[11];
+struct my_phrase
+{
+    string sort_phrase = "zzzzzzzzzzzzzzzzzz";
+    size_t appear_count = 0;
+};
+
+my_phrase ten_phrase[11];
+unordered_map<string, my_word>word_count;
+unordered_map<string, size_t>phrase_count;
+
+string transform_word(string raw_word)
+{
+    size_t len = raw_word.length();
+    string simple_word;
+    string temp_word = raw_word;
+    transform(temp_word.begin(), temp_word.end(), temp_word.begin(), ::tolower);
+    bool is_start = false;
+    for (int i = len - 1; i >= 0; i--)
+    {
+        if (isalpha(temp_word[i]))
+        {
+            is_start = true;
+            simple_word = temp_word.substr(0, i + 1);
+            break;
+        }
+    }
+    return simple_word;
 }
 
-//深度优先递归遍历当前目录下文件夹和文件及子文件夹和文件  
-void DfsFolder(string path, int layer)
+void EnterMap(string last_word, string current_word)
 {
-	_finddata_t file_info;
-	string current_path = path + "/*.*"; //也可以用/*来匹配所有  
-	intptr_t handle = _findfirst(current_path.c_str(), &file_info);
-	//返回值为-1则查找失败  
-	if (-1 == handle)
-	{
-		cout << "cannot match the path" << endl;
-		return;
-	}
+    string simple_last_word;
+    string simple_current_word;
+    size_t len = last_word.length();
+    string temp_word = last_word;
+    transform(temp_word.begin(), temp_word.end(), temp_word.begin(), ::tolower);
+    bool is_start = false;
+    for (size_t i = len - 1; i >= 0; i--)
+    {
+        if (isalpha(temp_word[i]))
+        {
+            is_start = true;
+            simple_last_word = temp_word.substr(0, i + 1);
+            break;
+        }
+    }
+    len = current_word.length();
+    temp_word = current_word;
+    transform(temp_word.begin(), temp_word.end(), temp_word.begin(), ::tolower);
+    is_start = false;
+    for (size_t i = len - 1; i >= 0; i--)
+    {
+        if (isalpha(temp_word[i]))
+        {
+            is_start = true;
+            simple_current_word = temp_word.substr(0, i + 1);
+            break;
+        }
+    }
+    word_count[simple_current_word].appear_count++;
+    if (current_word<word_count[simple_current_word].sort_word)
+    {
+        word_count[simple_current_word].sort_word = current_word;
+    }
 
-	do
-	{
-		//判断是否子目录  
-		if (file_info.attrib == _A_SUBDIR)
-		{
-			//递归遍历子目录  
-			//打印记号反映出深度层次  
-			for (int i = 0; i<layer; i++)
-				cout << "--";
-			cout << file_info.name << endl;
-			int layer_tmp = layer;
-			if (strcmp(file_info.name, "..") != 0 && strcmp(file_info.name, ".") != 0)  //.是当前目录，..是上层目录，必须排除掉这两种情况  
-				DfsFolder(path + '/' + file_info.name, layer_tmp + 1); //再windows下可以用\\转义分隔符，不推荐  
-		}
-		else
-		{
-			//打印记号反映出深度层次  
-			for (int i = 0; i<layer; i++)
-				cout << "--";
-			cout << file_info.name << endl;
-		}
-	} while (!_findnext(handle, &file_info));  //返回0则遍历完  
-											   //关闭文件句柄  
-	_findclose(handle);
+
+    string simple_phrase = simple_last_word + '_' + simple_current_word;
+    phrase_count[simple_phrase]++;
+
+}
+
+void NumOfCharsLinesInFile(string FileLocation)
+{//Read the file, count the number of characters, lines, and words, and add it to the global variable. The word is processed and added to the map dictionary.
+    //int NumberChars = 0;
+    int NumberLines = 1;
+    int NumberWords = 0;
+    char last_char = ' ';
+    char current_char;
+    bool wordbegin = false;
+    string current_word;
+    string last_word;
+
+    size_t sz;
+    FILE * fp = fopen(FileLocation.c_str(), "rb");
+    fseek(fp, 0L, SEEK_END);
+    sz = ftell(fp);
+
+    rewind(fp);
+    char*buf;
+    buf = (char*)malloc(sz * sizeof(char));
+    int len = fread(buf, sizeof(char), sz, fp);
+    //if (len) {
+    //	NumberLines++;
+    //}
+
+    for(int i = 0;i<len;i++)
+    {
+        current_char = buf[i];
+        if (current_char == '\n') {
+            NumberLines++;
+        }
+        if (current_char < 32 || current_char>126)
+        {
+            current_char = ' ';
+            TotalNum_chars--;
+        }
+        //判断是否为单词
+        if ((!isalpha(last_char)) && (!isdigit(last_char)) && (isalpha(current_char)))
+        {
+            wordbegin = true;
+            current_word = current_char;
+        }
+        else if (wordbegin)
+        {
+            if ((isalpha(current_char)) || (isdigit(current_char)))
+            {
+                //current_word.push_back(current_char);
+                current_word.push_back(current_char);
+                if (i == len-1) {
+                    goto panduan;
+                }
+            }
+            else
+            {
+                panduan:				wordbegin = false;
+                //Determines whether the current current word meets the word requirement: the first four characters are all letters
+                if (isalpha(current_word[1]) && isalpha(current_word[2]) && isalpha(current_word[3]))
+                {
+
+                    //that current_word meets the requirements
+                    NumberWords++;
+                    EnterMap(last_word, current_word);
+                    last_word = current_word;  //NumberWords++，word，last_word=current_word
+                    current_word.clear();
+
+                }
+            }
+        }
+        last_char = current_char;
+    }
+
+    free(buf);
+
+
+    TotalNum_chars += sz;
+    TotalNum_lines += NumberLines;
+    TotalNum_words += NumberWords;
+    fclose(fp);
+    fp = NULL;
+}
+
+//深度优先递归遍历当前目录下文件夹和文件及子文件夹和文件
+void listDir(char *path)  //main函数的argv[1] char * 作为 所需要遍历的路径 传参数给listDir
+{
+    DIR              *pDir ;  //定义一个DIR类的指针
+    struct dirent    *ent  ;   //定义一个结构体 dirent的指针，dirent结构体见上
+    int               i=0  ;
+    char              childpath[512];  //定义一个字符数组，用来存放读取的路径
+
+    pDir=opendir(path); //  opendir方法打开path目录，并将地址付给pDir指针
+    memset(childpath,0,sizeof(childpath)); //将字符数组childpath的数组元素全部置零
+
+
+    while((ent=readdir(pDir))!=NULL) //读取pDir打开的目录，并赋值给ent, 同时判断是否目录为空，不为空则执行循环体
+    {
+
+        if(ent->d_type & DT_DIR)  //读取 打开目录的文件类型 并与 DT_DIR进行位与运算操作，即如果读取的d_type类型为DT_DIR (=4 表示读取的为目录)
+        {
+
+            if(strcmp(ent->d_name,".")==0 || strcmp(ent->d_name,"..")==0)
+//如果读取的d_name为 . 或者.. 表示读取的是当前目录符和上一目录符, 用contiue跳过，不进行下面的输出
+                continue;
+
+            sprintf(childpath,"%s/%s",path,ent->d_name);  //如果非. ..则将 路径 和 文件名d_name 付给childpath, 并在下一行prinf输出
+            //printf("path:%s\n",childpath);
+
+            listDir(childpath);  //递归读取下层的字目录内容， 因为是递归，所以从外往里逐次输出所有目录（路径+目录名），
+//然后才在else中由内往外逐次输出所有文件名
+
+        }
+        else  //如果读取的d_type类型不是 DT_DIR, 即读取的不是目录，而是文件，则直接输出 d_name, 即输出文件名
+        {
+            //cout<<ent->d_name<<endl;  //cout<<childpath<<"/"<<ent->d_name<<endl;  输出文件名 带上了目录
+            sprintf(childpath,"%s/%s",path,ent->d_name);
+            NumOfCharsLinesInFile(childpath);
+
+        }
+    }
+
+}
+bool compare(my_word a, my_word b)
+{
+    return a.appear_count>b.appear_count;   //升序排列
+}
+
+bool phrase_compare(my_phrase a, my_phrase b)
+{
+    return a.appear_count>b.appear_count;   //升序排列
+}
+
+void Getten_word() {
+
+    my_word temporary_word;
+    for (const auto &w : word_count)
+    {
+        ten_word[10] = w.second;
+        for (int i = 0; i <= 9; i++)
+        {
+            if (ten_word[i].appear_count < ten_word[i + 1].appear_count)
+            {
+                temporary_word = ten_word[i];
+                ten_word[i] = ten_word[i + 1];
+                ten_word[i + 1] = temporary_word;
+            }
+        }
+    }
+    sort(ten_word, ten_word + 10, compare);
+}
+
+void Getten_phrase()
+{
+    my_phrase temporary_phrase;
+    for (const auto &w : phrase_count)
+    {
+        ten_phrase[10].appear_count = w.second;
+        ten_phrase[10].sort_phrase = w.first;
+        for (int i = 0; i <= 9; i++)
+        {
+            if (ten_phrase[i].appear_count < ten_phrase[i + 1].appear_count)
+            {
+                temporary_phrase = ten_phrase[i];
+                ten_phrase[i] = ten_phrase[i + 1];
+                ten_phrase[i + 1] = temporary_phrase;
+            }
+        }
+    }
+    sort(ten_phrase, ten_phrase + 10, phrase_compare);
 }
 
 int main(int argc, char *argv[])
 {
-	//遍历单个目录  
-	int file_num = 0;
-//	if (!TraverseFiles("D:/android-ndk", file_num))  //此处路径连接符只能用/，根盘符大小写都行  
-//		cout << "traverse files failed" << endl;
-//	cout << "-------------------\n" << "file number: " << file_num << endl;
+    clock_t tStart = clock();
+    listDir("/home/ruizhao/Documents/newsample");
+    Getten_word();
+    Getten_phrase();
+    ofstream result;
+    result.open("result.txt");
 
-	//递归遍历文件夹  
-	DfsFolder("D:/android-ndk", 0);
-	return 0;
+    result << "char_number :" << TotalNum_chars << endl;
+    result << "line_number :" << TotalNum_lines << endl;
+    result << "word_number :" << TotalNum_words << endl;
+
+    result <<endl<< "the top ten frequency of word : " << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        result << ten_word[i].sort_word << "  " << ten_word[i].appear_count << endl;
+
+    }
+
+    result <<"\n\n"<< "the top ten frequency of phrase :" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        string phrase_now = ten_phrase[i].sort_phrase;
+        string temp1, temp2;
+        int x = phrase_now.length();
+        int k = phrase_now.find("_");
+        string xx = phrase_now.substr(0, k);
+        result << word_count[phrase_now.substr(0, k)].sort_word << ' ' << word_count[phrase_now.substr(k + 1, x - k - 1)].sort_word <<" "<< ten_phrase[i].appear_count << endl;
+    }
+    result.close();
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+    return 0;
 }
